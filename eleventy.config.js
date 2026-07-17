45ea861ca7e0f031b47d5e945ef9a424c034cdae
igorflady <igor.flady@gmail.com>
2026-07-17 08:31:18 -0300

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,1 +1,12 @@
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("script.js");
+  eleventyConfig.addPassthroughCopy("imagens");
 
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+}
